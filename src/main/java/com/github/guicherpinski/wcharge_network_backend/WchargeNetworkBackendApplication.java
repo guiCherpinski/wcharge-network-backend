094f587feb7408d297233f7d49b809188d481e63
guiCherpinski <cherpinskiguilherme@gmail.com>
2026-09-24 16:30:53 -0300

    Primeiro commit

---
+++ src/main/java/com/github/guicherpinski/wcharge_network_backend/WchargeNetworkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.guicherpinski.wcharge_network_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WchargeNetworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WchargeNetworkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/guicherpinski/wcharge_network_backend/WchargeNetworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guicherpinski.wcharge_network_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WchargeNetworkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
