6dcda199ccc43083eef09ede14d67a8f4369eb82
guiCherpinski <cherpinskiguilherme@gmail.com>
2026-09-25 21:37:21 -0300

    feat: Cria securityFilter

---
+++ src/main/java/com/github/guicherpinski/wcharge_network_backend/security/SecurityFilter.java
@@ -0,0 +1,7 @@
+package com.github.guicherpinski.wcharge_network_backend.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityFilter {
+}
